1196e1da1c929cf630bf50e12a1f61cd24d1be6c
Alexander Smith <alex.smith@focusedsupport.com>
2024-03-29 09:06:59 -0600

    Added email validation to About and Careers page

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-function collapse(element) {
-    console.log(element.id);
-    let child = document.getElementById(element.id + "-child");
-    if (child.style.display === "block") {
-        child.style.display = "none";
-    }
-    else {
-        child.style.display = "block";
-    }
+let $emailInput = document.querySelector("#emailInput");
+$emailInput.addEventListener("change", validateEmailFormat())
+
+function validateEmailFormat() {
+  let isValid = emailIsValid($emailInput);
+  if (!isValid) {
+    $emailInput.setCustomValidity("Email must be formatted as name@domain.com");
+  }
+  else {
+    $emailInput.setCustomValidity("");
+  }
+}
+
+function emailIsValid(control) {
+  let email = control.value;
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
